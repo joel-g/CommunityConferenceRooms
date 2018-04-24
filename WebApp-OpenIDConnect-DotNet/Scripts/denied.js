9d594197456c47519e8aa7b9dcc87edcc67224ba
joel-g <joelaguerra1@gmail.com>
2018-04-24 10:39:01 -0700

    only gives authorized users the list

---
+++ WebApp-OpenIDConnect-DotNet/Scripts/denied.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () { 
+            $("#room-container").append(
+                "<div class='col-md-4'>\
+                 <h1>ACCESS DENIED</h1>\
+                 </div > ")
+});
--- WebApp-OpenIDConnect-DotNet/Scripts/rooms.js
+++ WebApp-OpenIDConnect-DotNet/Scripts/rooms.js
@@ -1,8 +1,6 @@
 ﻿$(document).ready(function () {
     var jqxhr = $.getJSON("Content/conferenceroom.json", function (r) {
-        console.log(r);
         for (let i = 0; i < r.length; i++) {
-            console.log(r[i].name);
             $("#room-container").append(
                 "<div class='col-md-4'>\
                         <h2>" + r[i].name + "</h2>\
