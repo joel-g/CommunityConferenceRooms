9176f19b22441e6d36ad972c6749bd9a61a82942
joel-g <joelaguerra1@gmail.com>
2018-04-24 10:49:00 -0700

    has working images and not completely broken style

--- WebApp-OpenIDConnect-DotNet/Scripts/denied.js
+++ WebApp-OpenIDConnect-DotNet/Scripts/denied.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () { 
             $("#room-container").append(
-                "<div class='col-md-4'>\
-                 <h1>ACCESS DENIED</h1>\
+                "<div><img src='/Content/UmbrellaCorporation.png' alt='Umbrella Corp logo' height='128' width='128' />\
+                 <h1><strong>ACCESS DENIED.</strong></h1>\
+                </h2>Unauthorized User Detected</h2>\
                  </div > ")
 });
