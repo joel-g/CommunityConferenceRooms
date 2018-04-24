26b24a93eae2f1f436f28cff92521ea8fd544e51
joel-g <joelaguerra1@gmail.com>
2018-04-24 15:17:10 -0700

    production

--- WebApp-OpenIDConnect-DotNet/Scripts/denied.js
+++ WebApp-OpenIDConnect-DotNet/Scripts/denied.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () { 
             $("#room-container").append(
-                "<div><img src='/Content/UmbrellaCorporation.png' alt='Umbrella Corp logo' height='128' width='128' />\
-                 <h1><strong>ACCESS DENIED.</strong></h1>\
-                </h2>Unauthorized User Detected</h2>\
+                "<div style='text-align:center'><img src='/Content/UmbrellaCorporation.png' alt='Umbrella Corp logo' height='212' width='212' />\
+                 <h1><strong>AUTHENTICATION REQUIRED.</h1>\
+                </h2><marquee>Please sign in to access confidiential data.</marquee></strong></h2>\
                  </div > ")
 });
--- WebApp-OpenIDConnect-DotNet/Scripts/rooms.js
+++ WebApp-OpenIDConnect-DotNet/Scripts/rooms.js
@@ -4,8 +4,8 @@
             $("#room-container").append(
                 "<div class='col-md-4'>\
                         <h2>" + r[i].name + "</h2>\
-                            <p>" + "MEALS" + "</p>\
-                            <p><a class='btn btn-default' href='" + r[i].links.google_maps.url + "'>Bing &raquo;</a>    <a class='btn btn-default' href='" + r[i].links.yelp.url + "'>Yelp &raquo;</a></p >\
+                            <p>Distance to building 42: <strong>" + r[i].distance_to_42 + " miles</strong></p>\
+                            <p><a class='btn btn-default' href='" + r[i].links.google_maps.url + "'>Map &raquo;</a>    <a class='btn btn-default' href='" + r[i].links.yelp.url + "'>Yelp &raquo;</a></p >\
                     </div >")
         }
     })
