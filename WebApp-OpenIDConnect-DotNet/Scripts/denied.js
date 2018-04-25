a5f2ba94fdc25873cad2492aefa49f51a20d8f1f
joel-g <joelaguerra1@gmail.com>
2018-04-25 16:28:29 -0700

    updates

--- WebApp-OpenIDConnect-DotNet/Scripts/denied.js
+++ WebApp-OpenIDConnect-DotNet/Scripts/denied.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () { 
             $("#room-container").append(
-                "<div style='text-align:center'><img src='/Content/UmbrellaCorporation.png' alt='Umbrella Corp logo' height='212' width='212' />\
+                "<div style='text-align:center'><img src='/Content/UmbrellaCorporation.png' alt='Umbrella Corp logo' height='164' width='164' />\
                  <h1><strong>AUTHENTICATION REQUIRED.</h1>\
-                </h2><marquee>Please sign in to access confidiential data.</marquee></strong></h2>\
+                </h2>Please sign in to access confidiential data.</strong></h2>\
                  </div > ")
 });
