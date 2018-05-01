ce8aab8be9dba2a63a81b0e41373676609dc6004
joel-g <joelaguerra1@gmail.com>
2018-05-01 16:31:57 -0700

    add off the rails

--- WebApp-OpenIDConnect-DotNet/Scripts/denied.js
+++ WebApp-OpenIDConnect-DotNet/Scripts/denied.js
@@ -2,6 +2,6 @@
             $("#room-container").append(
                 "<div style='text-align:center'><img src='/Content/UmbrellaCorporation.png' alt='Umbrella Corp logo' height='164' width='164' />\
                  <h1><strong>AUTHENTICATION REQUIRED.</h1>\
-                </h2>Please sign in to access confidiential data.</strong></h2>\
+                </h2>Please sign in to access confidential data.</strong></h2>\
                  </div > ")
 });
